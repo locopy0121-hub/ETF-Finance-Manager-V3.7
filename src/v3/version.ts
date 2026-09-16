818260057ad75a28afdf3a4f30139bd7633af2dd
locopy0121-hub <locopy0121@gmail.com>
2026-09-16 15:42:25 +0800

    chore(v3.7.5): update display build version

--- src/v3/version.ts
+++ src/v3/version.ts
@@ -1,4 +1,4 @@
-export const APP_SEMVER='3.7.4';
-export const APP_BUILD=42;
+export const APP_SEMVER='3.7.5';
+export const APP_BUILD=43;
 export const APP_DISPLAY_VERSION=`V${APP_SEMVER}`;
 export const APP_VERSION_LABEL=APP_DISPLAY_VERSION;
