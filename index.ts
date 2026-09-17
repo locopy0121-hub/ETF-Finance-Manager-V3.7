544003b4372853c31b4a872e60f51966f4dee86b
locopy0121-hub <locopy0121@gmail.com>
2026-09-17 10:16:28 +0800

    feat(monitor): mount monitor provider at app root

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import './src/services/backgroundCloseTask';
+import React from 'react';
 import { registerRootComponent } from 'expo';
 import { registerWidgetTaskHandler } from 'react-native-android-widget';
 import App from './App';
 import { widgetTaskHandler } from './src/widgets/widgetTaskHandler';
-registerRootComponent(App);
+import { MonitorProvider } from './src/context/MonitorContext';
+import { MONITOR_TEMPLATES } from './src/v3/monitorTemplates';
+
+function AppWithMonitorProvider() {
+  return React.createElement(
+    MonitorProvider,
+    { templates: MONITOR_TEMPLATES },
+    React.createElement(App),
+  );
+}
+
+registerRootComponent(AppWithMonitorProvider);
 registerWidgetTaskHandler(widgetTaskHandler);
