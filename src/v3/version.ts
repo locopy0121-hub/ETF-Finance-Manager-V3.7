7cac35451c8c4f732a33e0a1eec5ca1fbb48c300
locopy0121-hub <locopy0121@gmail.com>
2026-09-17 11:36:54 +0800

    release: bump app version to 3.7.7 build 45

--- src/v3/version.ts
+++ src/v3/version.ts
@@ -1,4 +1,4 @@
-export const APP_SEMVER='3.7.6';
-export const APP_BUILD=44;
+export const APP_SEMVER='3.7.7';
+export const APP_BUILD=45;
 export const APP_DISPLAY_VERSION=`V${APP_SEMVER}`;
 export const APP_VERSION_LABEL=APP_DISPLAY_VERSION;
