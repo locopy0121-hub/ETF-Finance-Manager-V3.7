9fbde676a9672deb7f19ed81cd0aced5f93b7049
locopy0121-hub <locopy0121@gmail.com>
2026-09-17 15:01:03 +0800

    chore(v3.7.8): align runtime version constants

--- src/v3/version.ts
+++ src/v3/version.ts
@@ -1,4 +1,4 @@
-export const APP_SEMVER='3.7.6';
-export const APP_BUILD=44;
+export const APP_SEMVER='3.7.8';
+export const APP_BUILD=46;
 export const APP_DISPLAY_VERSION=`V${APP_SEMVER}`;
 export const APP_VERSION_LABEL=APP_DISPLAY_VERSION;
